63f423fccd245f3f4171bef0fc734caf840c755d
JoshuaEagles <joshuaeagles1@gmail.com>
2020-10-22 10:34:16 -0400

    Switch client app.js to an IIFE

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,30 +1,32 @@
 /* app.js - Joshua Eagles - 301078033 - 2020-10-08 */
 
-let titleSuffix = " - Joshua Eagles Portfolio";
+(() => {
+	let titleSuffix = " - Joshua Eagles Portfolio";
 
-if (document.title == "Contact Me" + titleSuffix)
-{
-	let submitButton = document.getElementById("submit");
-
-	// Adds an event listener for the submit button that will print the form to the console and then redirect to the home page
-	submitButton.addEventListener("click", (event) => 
+	if (document.title == "Contact Me" + titleSuffix)
 	{
-		event.preventDefault();
+		let submitButton = document.getElementById("submit");
 
-		let firstName = document.getElementById("firstName").value;
-		let lastName = document.getElementById("lastName").value;
-		let contactNumber = document.getElementById("contactNumber").value;
-		let email = document.getElementById("email").value;
-		let message = document.getElementById("message").value;
+		// Adds an event listener for the submit button that will print the form to the console and then redirect to the home page
+		submitButton.addEventListener("click", (event) => 
+		{
+			event.preventDefault();
 
-		let returnMessage = "First Name: " + firstName;
-		returnMessage += "\nLast Name: " + lastName;
-		returnMessage += "\nContact Number: " + contactNumber;
-		returnMessage += "\nEmail: " + email;
-		returnMessage += "\nMessage: " + message;
+			let firstName = document.getElementById("firstName").value;
+			let lastName = document.getElementById("lastName").value;
+			let contactNumber = document.getElementById("contactNumber").value;
+			let email = document.getElementById("email").value;
+			let message = document.getElementById("message").value;
 
-		window.alert(returnMessage);
+			let returnMessage = "First Name: " + firstName;
+			returnMessage += "\nLast Name: " + lastName;
+			returnMessage += "\nContact Number: " + contactNumber;
+			returnMessage += "\nEmail: " + email;
+			returnMessage += "\nMessage: " + message;
 
-		window.location.href="/home";
-	});
-}
+			window.alert(returnMessage);
+
+			window.location.href="/home";
+		});
+	}
+})();
